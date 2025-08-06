84a8a28cbf1598bfdf870cc6ea8ced8809e2176c
praise456 <35904244+praise456@users.noreply.github.com>
2025-08-06 04:14:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://crypto-backend-t3bz.onrender.com//api";
 
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.getElementById("loginForm");
